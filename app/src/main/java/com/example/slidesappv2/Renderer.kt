62d7b5751203574f2ded0a74c0aad046c9efc6e0
Tyler Caslin <tylercaslin@8afbff1b.st-andrews.ac.uk>
2020-02-15 17:24:17 +0000

    selection and deselection

--- app/src/main/java/com/example/slidesappv2/Renderer.kt
+++ app/src/main/java/com/example/slidesappv2/Renderer.kt
@@ -15,11 +15,16 @@ import androidx.recyclerview.widget.RecyclerView
 
 class Renderer(private val mainActivity: MainActivity, private val view: View) {
 
+    private val unselected = -1
+    private val undo = "UNDO"
+
     internal fun render(parcelFileDescriptor: ParcelFileDescriptor) {
         val renderer = PdfRenderer(parcelFileDescriptor)
         val scrollPane = mainActivity.findViewById<HorizontalScrollView>(R.id.scroll)
         val options = LinearLayout(mainActivity)
         val pageCount = renderer.pageCount
+        val images = ArrayList<ImageView>()
+        var indexSelected = unselected
 
         for (i in 0 until pageCount) {
             val page = renderer.openPage(i)
@@ -40,23 +45,58 @@ class Renderer(private val mainActivity: MainActivity, private val view: View) {
             page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
 
             imageView.setImageBitmap(bitmap)
+            images.add(imageView)
             imageView.invalidate() // redraw
 
             page.close() // done with page
 
             imageView.setOnClickListener {
-                mainActivity.showToast(i.toString())
-                // check if already selected
-                imageView.setColorFilter(Color.LTGRAY, PorterDuff.Mode.DARKEN)
-
+                val tempIndex = indexSelected
+                if (indexSelected == i) {
+                    // already selected, deselect
+                    indexSelected = unselected
+                    deselectImage(imageView)
+                    mainActivity.showSnackbar(view, "Deselected slide $i.", null, undo,
+                        View.OnClickListener {
+                            selectImage(imageView)
+                            indexSelected = tempIndex
+                        }
+                    )
+                    return@setOnClickListener
+                }
+                if (indexSelected != unselected) {
+                    images[indexSelected].clearColorFilter() // clear previous selection
+                }
+                indexSelected = i
+                selectImage(imageView)
+                mainActivity.showSnackbar(view, "Selected slide $i.", null, undo,
+                    View.OnClickListener {
+                        deselectImage(imageView)
+                        indexSelected = tempIndex
+                        if (indexSelected != unselected) {
+                            selectImage(images[indexSelected])
+                        }
+                    }
+                )
             }
             options.addView(imageView)
         }
         renderer.close()
         scrollPane.removeAllViews()
         scrollPane.addView(options)
-        scrollPane.scrollTo(0, 0)
+        scrollPane.scrollTo(0, 0) // reset to beginning
         scrollPane.invalidate()
     }
+
+    private fun selectImage(imageView: ImageView) {
+        imageView.setColorFilter(Color.LTGRAY, PorterDuff.Mode.DARKEN)
+        imageView.invalidate()
+    }
+
+    private fun deselectImage(imageView: ImageView) {
+        imageView.clearColorFilter()
+        imageView.invalidate()
+    }
+
 
 }
